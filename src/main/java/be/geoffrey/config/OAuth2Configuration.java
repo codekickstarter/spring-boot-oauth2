d52dd8cfc3b0db63aca3808c985722dd12f58a20
Geoffrey De Vylder <geoffreydv@gmail.com>
2016-01-25 20:26:44 +0100

    Added client_id and client_secret as a possibility to logon

--- src/main/java/be/geoffrey/config/OAuth2Configuration.java
+++ src/main/java/be/geoffrey/config/OAuth2Configuration.java
@@ -2,7 +2,6 @@ package be.geoffrey.config;
 
 import be.geoffrey.security.Authorities;
 import be.geoffrey.security.CustomAuthenticationEntryPoint;
-import be.geoffrey.security.CustomLogoutSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.bind.RelaxedPropertyResolver;
@@ -10,18 +9,20 @@ import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.client.ClientCredentialsTokenEndpointFilter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.sql.DataSource;
 
@@ -32,35 +33,30 @@ public class OAuth2Configuration {
     @EnableResourceServer
     protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
-        @Autowired
-        private CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
-
-        @Autowired
-        private CustomLogoutSuccessHandler customLogoutSuccessHandler;
-
         @Override
         public void configure(HttpSecurity http) throws Exception {
 
-            http
-                    .exceptionHandling()
-                    .authenticationEntryPoint(customAuthenticationEntryPoint)
-                    .and()
-                    .logout()
-                    .logoutUrl("/oauth/logout")
-                    .logoutSuccessHandler(customLogoutSuccessHandler)
-                    .and()
-                    .csrf()
-                    .requireCsrfProtectionMatcher(new AntPathRequestMatcher("/oauth/authorize"))
-                    .disable()
-                    .headers()
-                    .frameOptions().disable()
-                    .sessionManagement()
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                    .and()
-                    .authorizeRequests()
-                    .antMatchers("/hello/**").permitAll()
-                    .antMatchers("/secure/**").authenticated();
+            // TODO: Add back example for csrf
 
+            http
+//                    .exceptionHandling()
+//                    .authenticationEntryPoint(customAuthenticationEntryPoint)
+//                    .and()
+//                    .logout()
+//                    .logoutUrl("/oauth/logout")
+//                    .logoutSuccessHandler(customLogoutSuccessHandler)
+//                    .and()
+//                    .csrf()
+//                    .requireCsrfProtectionMatcher(new AntPathRequestMatcher("/oauth/authorize"))
+//                    .disable()
+//                    .headers()
+//                    .frameOptions().disable()
+//                    .sessionManagement()
+//                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                    .and()
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.OPTIONS, "/oauth/token").permitAll()
+                    .antMatchers("/**").permitAll();
         }
 
     }
@@ -78,15 +74,14 @@ public class OAuth2Configuration {
 
         @Autowired
         private DataSource dataSource;
+        @Autowired
+        @Qualifier("authenticationManagerBean")
+        private AuthenticationManager authenticationManager;
 
         @Bean
         public TokenStore tokenStore() {
             return new JdbcTokenStore(dataSource);
         }
-
-        @Autowired
-        @Qualifier("authenticationManagerBean")
-        private AuthenticationManager authenticationManager;
 
         @Override
         public void configure(AuthorizationServerEndpointsConfigurer endpoints)
@@ -101,11 +96,17 @@ public class OAuth2Configuration {
             clients
                     .inMemory()
                     .withClient(propertyResolver.getProperty(PROP_CLIENTID))
+                    .secret(propertyResolver.getProperty(PROP_SECRET))
                     .scopes("read", "write")
                     .authorities(Authorities.ROLE_ADMIN.name(), Authorities.ROLE_USER.name())
                     .authorizedGrantTypes("password", "refresh_token")
-                    .secret(propertyResolver.getProperty(PROP_SECRET))
                     .accessTokenValiditySeconds(propertyResolver.getProperty(PROP_TOKEN_VALIDITY_SECONDS, Integer.class, 1800));
+        }
+
+        // Het oauth/xxx endpoint wordt via deze config beveiligd
+        @Override
+        public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+            security.allowFormAuthenticationForClients();
         }
 
         @Override
--- src/main/java/be/geoffrey/config/SecurityConfiguration.java
+++ src/main/java/be/geoffrey/config/SecurityConfiguration.java
@@ -3,18 +3,15 @@ package be.geoffrey.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
-import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -30,23 +27,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-
         auth
                 .userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder());
-
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-
-        web
-                .ignoring()
-                .antMatchers("/h2console/**")
-                .antMatchers("/api/register")
-                .antMatchers("/api/activate")
-                .antMatchers("/api/lostpassword")
-                .antMatchers("/api/resetpassword");
+        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/oauth/token");
     }
 
     @Override
@@ -54,15 +42,5 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
-
-    @EnableGlobalMethodSecurity(prePostEnabled = true, jsr250Enabled = true)
-    private static class GlobalSecurityConfiguration extends GlobalMethodSecurityConfiguration {
-        @Override
-        protected MethodSecurityExpressionHandler createExpressionHandler() {
-            return new OAuth2MethodSecurityExpressionHandler();
-        }
-
-    }
-
 }
 
